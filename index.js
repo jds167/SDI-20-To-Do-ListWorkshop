301ec24f489dd4dd23b21cb242ab0013988e5111
Jasmine <jds167@students.uwf.edu>
2023-09-13 17:37:02 -0400

    Added delete button and fixed color

--- index.js
+++ index.js
@@ -2,19 +2,50 @@ let input = document.getElementById("todoInput");
 let addButton = document.getElementById("addTodo");
 let list = document.getElementById("todoList")
 let inputValue = "";
+let taskCount = 0;
+
+function addTaskItem () {
+    input.value = "";
+    let listElement = document.createElement("li");
+    listElement.id = taskCount++;
+    // Put style on CSS instead
+
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.onchange = toggleTask;
+
+    listElement.appendChild(checkBox);
+    listElement.appendChild(document.createTextNode(inputValue));
+    list.appendChild(listElement);
+}
 
 input.addEventListener("keyup", (eventObj) => {
     inputValue = eventObj.target.value;
-    if (eventObj.keyword == 13) {
-        list.innerHTML += '<li> ${item} </li>'
-    }
-    // console.log('return ' + inputValue);
-    return inputValue;
+    // if (eventObj.keyword == 13) {
+    //     list.innerHTML += '<li> ${item} </li>'
+    // }
+    
 })
 
-addButton.addEventListener('click', () => {
-    let listItem = document.createElement("li");
-    listItem.innerHTML = `${inputValue} <input type = "checkbox">`;
-    list.appendChild(listItem);
-})
+addButton.addEventListener('click', addTaskItem);
+
+function toggleTask () {
+    let task = document.getElementById(this.parentNode.id);
+    if (this.checked)
+    {
+        task.style.textDecoration = 'line-through';
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = deleteTask;
+        task.appendChild(deleteButton);
+
+    } else {
+        task.style.textDecoration = 'none';
+    }
+}
+
+function deleteTask() {
+    let task = document.getElementById(this.parentNode.id);
+    task.remove();
+}
 
